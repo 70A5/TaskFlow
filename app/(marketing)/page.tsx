9cbb9d18611136354010c3694c6bff2a7d4cc644
70A5 <joas.van.der.eerden@outlook.com>
2024-01-16 12:49:24 +0100

    homepage update
    vergeet niet om een like achter te laten en tot in de volgende video

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import localFont from "next/font/local";
 import { Poppins } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Medal } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const headingFont = localFont({
-  src: "../../public/fonts/font.woff2"
+  src: "../../public/fonts/Inter-Bold.woff2"
 });
 
-const textFont = Poppins({
+const textFont = Inter({
   subsets: ["latin"],
   weight: [
     "100",
@@ -27,33 +28,43 @@ const textFont = Poppins({
 
 const MarketingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className={cn(
-        "flex items-center justify-center flex-col",
-        headingFont.className,
-      )}>
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2" />
-          Glazen Bol Awards 2023
+    <div className="bg-[url('../public/wallpaper.jpg')] w-screen h-screen -mt-28 pt-16">
+      <div className="flex items-center justify-center flex-col">
+        <div className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className,
+        )}>
+          {/* <div className="mb-4 flex items-center border shadow-sm p-4 bg-emerald-100 text-emerald-700 rounded-full uppercase">
+            <Medal className="h-6 w-6 mr-2" />
+            Glazen Bol Awards 2023
+          </div> */}
+          <div className="flex">
+            <Image className="mb-28 pt-12"
+              src={"/logo1.png"}
+              alt="Logo"
+              height={100}
+              width={150}
+            />
+            <h1 className="text-3xl md:text-6xl text-center text-white mt-14 mb-6">
+              TaskFlow
+            </h1>
+          </div>
+          <div className="text-3xl md:text-6xl bg-gradient-to-r from-emerald-500 to-blue-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+            Verdeel taken, beter.
+          </div>
         </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-          TaskFlow
-        </h1>
-        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          Stijg samen op
+        <div className={cn(
+          "text-sm md:text-xl text-white mt-14 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className,
+        )}>
+          TaskFlow is een gratis en open source project management tool. Maak een account aan en begin vandaag nog met het beter verdelen van taken.
         </div>
+        <Button className="mt-6" size="lg" asChild>
+          <Link href="/sign-up">
+            Start nu gratis
+          </Link>
+        </Button>
       </div>
-      <div className={cn(
-        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
-        textFont.className,
-      )}>
-        TaskFlow is een online tool voor het beheren van projecten, taken en meer.
-      </div>
-      <Button className="mt-6" size="lg" asChild>
-        <Link href="/sign-up">
-          Start nu gratis
-        </Link>
-      </Button>
     </div>
   );
 };
--- components/logo.tsx
+++ components/logo.tsx
@@ -13,13 +13,13 @@ export const Logo = () => {
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
         <Image
-          src="/logo.svg"
+          src="/logo1.png"
           alt="Logo"
-          height={30}
-          width={30}
+          height={50}
+          width={50}
         />
         <p className={cn(
-          "text-lg text-neutral-700 pb-1",
+          "text-2xl text-neutral-700 pb-1",
           headingFont.className,
         )}>
           TaskFlow
